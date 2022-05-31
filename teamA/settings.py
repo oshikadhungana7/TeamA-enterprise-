4de45b825f2afa493b2e1e58dffc045f416fb754
Vidwat Dahal <dahalvidwat403@gmail.com>
2022-05-31 23:38:22 +0545

    New Base

--- teamA/settings.py
+++ teamA/settings.py
@@ -79,27 +79,27 @@ WSGI_APPLICATION = 'teamA.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'vrent',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
-
 # DATABASES = {
 #      'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'CarKhaani',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Loki123',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'vrent',
+#         'USER': 'root',
+#         'PASSWORD': '',
 #         'HOST': '127.0.0.1',
-#         'PORT': '5432',
+#         'PORT': '3306',
 #     }
 # }
+
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CarKhaani',
+        'USER': 'postgres',
+        'PASSWORD': 'Loki123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
